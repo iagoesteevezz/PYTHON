c1e4597bed675e4d8e19f77c78f62d47cab877c5
Iago Estévez <noa.arqnatura@gmail.com>
2026-04-09 19:49:07 +0200

    final del 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,1 +1,9 @@
 num = int(input("Introduce un número:"))
+cont = 0
+for i in range(num**num):
+    if num % i == 0:
+        cont +=1
+if cont > 2:
+    print("El número no es primo")
+else:
+    print("El número es primo")
